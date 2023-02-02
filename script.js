777a4b83e288b08c32d979f6bd5d185d43c4e8e1
amyskaletzky <amy.skaletzky@gmail.com>
2023-02-02 14:31:45 +0200

    trying to make responsive container

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict'
+const containerWidth = 100
+const containerHeight = 30
+const ground = document.querySelector('.ground')
+const rabbit = document.querySelector('.rabbit')
+
+resizeEverything()
+window.addEventListener('resize', resizeEverything)
+const container = document.querySelector('.container')
+// function resizeGround() {
+//     console.log('hi');
+//     ground.style.width = window.innerWidth + 'px'
+//     rabbit.style.width = (window.innerWidth * 0.1) + 'px'
+//     rabbit.style.height = (window.innerHeight * 0.1) + 'px'
+// }
+
+
+function resizeEverything() {
+    let containerToPixelScale
+    if (window.innerWidth / window.innerHeight < containerWidth / containerHeight) {
+        containerToPixelScale = window.innerWidth / containerWidth
+    } else {
+        containerToPixelScale = window.innerHeight / containerHeight
+    }
+
+    container.style.width = `${containerWidth * containerToPixelScale}px`
+    container.style.height = `${containerHeight * containerToPixelScale}px`
+
+}
