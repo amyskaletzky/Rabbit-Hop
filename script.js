7d036fd537808ba4f42618d3145dd7e0e0ada26a
KerenPetras <jackqlinep@gmail.com>
2023-02-03 21:56:31 +0200

    movingGround update

--- script.js
+++ script.js
@@ -40,32 +40,42 @@ function update(timeNow) {
 
 window.requestAnimationFrame(update) 
 
-
 //movingGround --Keren's part
-const groundElements = document.querySelectorAll("[date-groundMoves]")
 const speed = .05;
+const groundElements = document.querySelectorAll("[date-groundMoves]")
+extendGround();
 
 //In this function we are taking the current positing and updating it
 ///by taking a value from a css and converting it into a js value and back again to css variable
-function updatedMovingGround(diff) {
-    groundElements.forEach(ground /*from the css*/ => {
-    finalIncrementCustomProp(ground, "--left",diff * speed * -1)
-});
-} 
+
 
 //Due to lines 46,47 we have to make 3 functions
 //because everything on css is a string we create a function that convert to a float(number)
 /// and if there is not value just defult '0'
 
 function getCustomProp(elem, prop) {
-return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
+    return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
 }
-
 //After converting the values to float we need to use the computerd values
 function setCustomProp(elem, prop, value) {
-elem.style.setProperty(prop, value)
+    elem.style.setProperty(prop, value)
 }
 
 function finalIncrementCustomProp(elem, prop, inc){
     setCustomProp(elem,prop,getCustomProp(elem,prop) + inc)
+}
+
+function updatedMovingGround(diff) { ///moves to the left every single time
+    groundElements.forEach(ground => {
+        finalIncrementCustomProp(ground, "--left",diff * speed * -1)
+
+    if (getCustomProp( ground, "--left")<= -200){
+        finalIncrementCustomProp(ground, "--left", 400)
+    }
+});
+} 
+
+function extendGround() {
+    setCustomProp(groundElements[0], "--left", 0)
+    setCustomProp(groundElements[1], "--left",containerWidth, 200)    
 }
